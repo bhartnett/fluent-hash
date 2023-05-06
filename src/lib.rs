5f188b3a16706fec86c4a5c67691b141153aef38
Web3 Developer <51288821+web3-developer@users.noreply.github.com>
2023-05-07 06:35:46 +0800

    Update author.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-// Copyright 2023 Web3 Developer - Web3Developer.io
+// Copyright 2023 Web3 Developer @ Web3Developer.io
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
